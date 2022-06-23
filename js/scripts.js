ed6dfec65b6e679a8a650e04ae923c23a1f511c9
makkoo0507 <makkoo0507@gmail.com>
2022-06-23 21:55:22 +0900

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+$(function() {
+    $('nav').hide();
+    
+    $('.burgerMenu').click(function(){
+        $('.bar').eq('0').toggleClass('barTop');
+        $('.bar').eq(1).toggleClass('barMiddle');
+        $('.bar').eq(2).toggleClass('barBottom');
+        $('nav').fadeToggle(700);
+        $('.navRight').toggleClass('open')
+        if($('.navRight').hasClass('open')){
+            $('.navRight').animate({right:0},600)
+        }else{
+            $('.navRight').animate({right:'-40%'},600)
+        }
+    });
+    $('.navItem').click(function(){
+        $('.bar').eq('0').toggleClass('barTop');
+        $('.bar').eq(1).toggleClass('barMiddle');
+        $('.bar').eq(2).toggleClass('barBottom');
+        $('nav').fadeToggle(700);
+        $('.navRight').toggleClass('open')
+        if($('.navRight').hasClass('open')){
+            $('.navRight').animate({right:0},600)
+        }else{
+            $('.navRight').animate({right:'-40%'},600)
+        }
+    })
+
+});
